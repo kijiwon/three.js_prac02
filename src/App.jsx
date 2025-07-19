b6cd1a9931cb46f7e550dc9608df8947ff9d3ceb
kijiwon <kiji1313@gmail.com>
2025-07-19 19:46:44 +0900

    tailwindcss 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,8 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import "./App.css";
-import { OrbitControls, Sparkles } from "@react-three/drei";
-import { useRef } from "react";
-
-const RotatingCube = () => {
-  const meshRef = useRef();
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01;
-      meshRef.current.rotation.x += 0.01;
-    }
-  });
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1, 1, 1, 32]} />
-      <meshLambertMaterial color={0x468585} emissive={0x468585} />
-      <Sparkles
-        count={100}
-        scale={1}
-        size={6}
-        speed={0.002}
-        noise={0.2}
-        color="orange"
-      />
-    </mesh>
-  );
-};
-
 function App() {
   return (
-    <Canvas
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-      {/* canvas 색상 설정 */}
-      <color attach="background" args={["#f0f0f0"]} />
-      <RotatingCube />
-    </Canvas>
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white underline">Hello</h1>
+    </main>
   );
 }
 
